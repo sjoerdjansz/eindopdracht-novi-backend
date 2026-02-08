1b9b484c85de970adf286391022edc76883b60b3
sjoerdjansz <sjoerdjansz@gmail.com>
2026-02-08 14:04:29 +0100

    spring boot project

---
+++ src/main/java/nl/sweatdaddy/SweatDaddyApplication.java
@@ -0,0 +1,13 @@
+package nl.sweatdaddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweatDaddyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SweatDaddyApplication.class, args);
+  }
+
+}
